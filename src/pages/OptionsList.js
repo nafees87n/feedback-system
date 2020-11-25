6c84df6ad4322b2e7e43a019560fb28cc07e947f
Nafees Nehar <nafees87n@gmail.com>
2020-11-26 00:13:53 +0530

    feat: modification

--- src/pages/OptionsList.js
+++ src/pages/OptionsList.js
@@ -1,20 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Grid,
   Paper,
   ButtonGroup,
+  TextField,
 } from '@material-ui/core';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import db from '../firebase';
 
 const Options = ({ feedback, id, count }) => {
-  function handleDelete() {
+  const [open, setOpen] = useState(false);
+  const [modified, setModified] = useState('');
+
+  const handleDelete = () => {
     db.collection('feedbacks').doc(id).delete();
-  }
+  };
+
+  const handleEdit = () => {
+    db.collection('feedbacks').doc(id).update({
+      feedback: modified,
+    });
+    setOpen(false);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+        maxWidth="sm"
+        fullWidth
+      >
+        <DialogTitle id="form-dialog-title">
+          Modify The Option
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Edited Option"
+            fullWidth
+            onChange={(e) => setModified(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleEdit} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <Grid item xs={8}>
         <Paper
           style={{ padding: '10px' }}
@@ -41,7 +95,7 @@ const Options = ({ feedback, id, count }) => {
           aria-label="contained primary button group"
           size="small"
         >
-          <Button>
+          <Button onClick={handleClickOpen}>
             <EditIcon />
           </Button>
           <Button onClick={handleDelete}>
